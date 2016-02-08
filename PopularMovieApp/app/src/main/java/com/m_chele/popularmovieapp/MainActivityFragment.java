ff953b6e00d35ee42b55262898b06a5b8d2068fe
m-chele <michele.cenere@gmail.com>
2016-02-08 14:59:56 +0100

    refactoring

--- PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/MainActivityFragment.java
+++ PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/MainActivityFragment.java
@@ -30,34 +30,8 @@ public class MainActivityFragment extends Fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         GridView filmsGridView = (GridView) rootView.findViewById(R.id.films_gridview);
-        BaseAdapter filmsAdapter = new BaseAdapter()
-        {
-            @Override
-            public int getCount()
-            {
-                return 4;
-            }
+        FilmsAdapter filmsAdapter = new FilmsAdapter(getContext());
 
-            @Override
-            public Object getItem(int i)
-            {
-                return "Pippo " + i;
-            }
-
-            @Override
-            public long getItemId(int i)
-            {
-                return 0;
-            }
-
-            @Override
-            public View getView(int i, View view, ViewGroup viewGroup)
-            {
-                TextView textView = new TextView(getContext());
-                textView.setText(getItem(i) + "");
-                return textView;
-            }
-        };
         filmsGridView.setAdapter(filmsAdapter);
 
 
