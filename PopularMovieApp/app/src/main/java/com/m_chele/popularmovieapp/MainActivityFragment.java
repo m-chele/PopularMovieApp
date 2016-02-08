c6869a9bc64d56880c02e69627798ca8ba7db9c2
m-chele <michele.cenere@gmail.com>
2016-02-08 14:49:26 +0100

    added gridview with dummy values

--- PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/MainActivityFragment.java
+++ PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/MainActivityFragment.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -20,6 +27,40 @@ public class MainActivityFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView filmsGridView = (GridView) rootView.findViewById(R.id.films_gridview);
+        BaseAdapter filmsAdapter = new BaseAdapter()
+        {
+            @Override
+            public int getCount()
+            {
+                return 4;
+            }
+
+            @Override
+            public Object getItem(int i)
+            {
+                return "Pippo " + i;
+            }
+
+            @Override
+            public long getItemId(int i)
+            {
+                return 0;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup)
+            {
+                TextView textView = new TextView(getContext());
+                textView.setText(getItem(i) + "");
+                return textView;
+            }
+        };
+        filmsGridView.setAdapter(filmsAdapter);
+
+
+        return rootView;
     }
 }
