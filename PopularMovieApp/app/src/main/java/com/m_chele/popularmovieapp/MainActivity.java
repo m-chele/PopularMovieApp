c2846bdd1fa584d62bc747a30c07c7a863c2dcee
m-chele <michele.cenere@gmail.com>
2016-03-09 14:57:12 +0100

    back button works properly

--- PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/MainActivity.java
+++ PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/MainActivity.java
@@ -40,21 +40,29 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings)
         {
             getFragmentManager()
                     .beginTransaction()
                     .replace(R.id.fragment, settingsFragment)
+                    .addToBackStack(null) // != null should be used for fragments without UI
                     .commit();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onBackPressed()
+    {
+        if (getFragmentManager().getBackStackEntryCount() > 0)
+        {
+            getFragmentManager().popBackStack();
+        } else
+        {
+            super.onBackPressed();
+        }
     }
 }
