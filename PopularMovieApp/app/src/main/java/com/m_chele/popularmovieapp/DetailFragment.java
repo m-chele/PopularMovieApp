095f6ef9b022aebfab74f8cd59191b198486e974
m-chele <michele.cenere@gmail.com>
2016-03-16 15:33:52 +0100

    ImageView used in detail

--- PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/DetailFragment.java
+++ PopularMovieApp/app/src/main/java/com/m_chele/popularmovieapp/DetailFragment.java
@@ -6,7 +6,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailFragment extends Fragment
 {
@@ -28,8 +31,15 @@ public class DetailFragment extends Fragment
         ((TextView) rootView.findViewById(R.id.film_title_textview))
                 .setText(filmModel.getString(getString(R.string.key_original_title)));
 
-        ((TextView) rootView.findViewById(R.id.film_poster_textview))
-                .setText(filmModel.getString(getString(R.string.key_poster_path)));
+        ImageView imageView = ((ImageView) rootView.findViewById(R.id.film_poster_textview));
+
+        Picasso.with(getActivity())
+                .load(getImageUrlForFilm())
+                .placeholder(R.drawable.film_placeholder)
+                .error(R.drawable.image_unavailable)
+                .fit()
+                .into(imageView);
+
 
         ((TextView) rootView.findViewById(R.id.film_overview_textview))
                 .setText(filmModel.getString(getString(R.string.key_overview)));
@@ -38,5 +48,16 @@ public class DetailFragment extends Fragment
                 .setText(filmModel.getString(getString(R.string.key_release_date)));
 
         return rootView;
+    }
+
+
+    // TODO: duplicated with FilmAdapter
+    private String getImageUrlForFilm()
+    {
+        final String IMAGE_BASE_URI = "http://image.tmdb.org/t/p";
+        final String IMAGE_SIZE = "/w185";
+
+        return IMAGE_BASE_URI + IMAGE_SIZE +
+                filmModel.getString(getActivity().getString(R.string.key_poster_path));
     }
 }
